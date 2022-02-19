abcd47008f787e1337c428f55646048ea164e07e
Lucas <lucas.prog07@gmail.com>
2022-02-19 12:04:38 -0300

    show passwords only

--- src/components/dashboard/Sidebar.vue
+++ src/components/dashboard/Sidebar.vue
@@ -14,12 +14,12 @@ const userName = loginData ? loginData.user : '';
 
 const items : ISidebarItem[] = [
   { name: t('passwords.title'), icon: 'passwords', route: 'dashboard-passwords', disabled: false },
-  /* { name: t('documents.title'), icon: 'documents', route: 'dashboard-documents', disabled: true },
-  { name: t('bank-accounts.title'), icon: 'bank-accounts', route: 'dashboard-bankaccounts', disabled: true },
-  { name: t('credit-cards.title'), icon: 'credit-cards', route: 'dashboard-creditcards', disabled: true },
-  { name: t('bitcoin-wallet.title'), icon: 'wallet', route: 'dashboard-wallet', disabled: true }, */
-  { name: t('emails.title'), icon: 'emails', route: 'dashboard-emails', disabled: true },
-  { name: t('notes.title'), icon: 'notes', route: 'dashboard-notes', disabled: true },
+  // { name: t('documents.title'), icon: 'documents', route: 'dashboard-documents', disabled: true },
+  // { name: t('bank-accounts.title'), icon: 'bank-accounts', route: 'dashboard-bankaccounts', disabled: true },
+  // { name: t('credit-cards.title'), icon: 'credit-cards', route: 'dashboard-creditcards', disabled: true },
+  // { name: t('bitcoin-wallet.title'), icon: 'wallet', route: 'dashboard-wallet', disabled: true },
+  // { name: t('emails.title'), icon: 'emails', route: 'dashboard-emails', disabled: true },
+  // { name: t('notes.title'), icon: 'notes', route: 'dashboard-notes', disabled: true },
 ];
 </script>
 
