d823a234ea1cc5ffc774572a4d6a7080c6be00be
Lucas <lucas.prog07@gmail.com>
2022-02-19 17:11:44 -0300

    home remade

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,36 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from '../components/HelloWorld.vue';
+import router from '../router';
+import { onMounted } from "@vue/runtime-core";
+
+onMounted(() => {
+  router.push({ name: 'login'});
+})
 </script>
 
 <template>
-  <div class="w-full flex flex-col items-center">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Hello vue" />
+  <div
+    class="
+      h-screen
+      w-screen
+      flex
+      justify-center
+      items-center bg-gradient-to-br from-gray-600 to-gray-900
+      flex-col
+    "
+  >
+    <h1 class="text-gray-50 text-4xl">{{ $t('wait') }}...</h1>
+    <h4 class="text-gray-300 text-sm mt-4">
+      {{ $t('not-redirected-message') }}
+      <router-link :to="{ name: 'login' }" class="hover:underline">
+        {{ $t('click-here') }}
+      </router-link>
+    </h4>
   </div>
 </template>
 
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
