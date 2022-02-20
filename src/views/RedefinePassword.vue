a5409b9e28f2c0c0a1e447483b3b7eb70a236965
Lucas <lucas.prog07@gmail.com>
2022-02-19 21:46:19 -0300

    fix redefine password

--- src/views/RedefinePassword.vue
+++ src/views/RedefinePassword.vue
@@ -1,18 +1,14 @@
 <script setup lang="ts">
-import { onMounted } from '@vue/runtime-core';
 import router from '../router';
 import LoginScreen from '../components/login/LoginScreen.vue';
 import ResetPasswordForm from '../components/login/ResetPasswordForm.vue';
 </script>
 
 <template>
-  <LoginScreen
-    :title="$t('reset-password.form.submit')"
-    v-if="$route.params.email && $route.params.token"
-  >
+  <LoginScreen :title="$t('reset-password.form.submit')">
     <ResetPasswordForm
-      :email="$route.params.email"
-      :token="$route.params.token"
+      :email="$route.params.email.toString()"
+      :token="$route.params.token.toString()"
       @reset="router.push({ name: 'login' })"
     />
   </LoginScreen>
