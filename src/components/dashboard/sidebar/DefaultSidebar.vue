635593d79f8353cdf94f3a88f5b38a3400fd83ef
lucas.souza <lucas.souza@bluecrux.com>
2022-02-22 11:30:13 -0300

    new images

--- src/components/dashboard/sidebar/DefaultSidebar.vue
+++ src/components/dashboard/sidebar/DefaultSidebar.vue
@@ -30,12 +30,13 @@ const menuHeight = computed(() => {
   >
     <div>
       <div ref="topEl" class="flex items-center justify-center flex-shrink-0 text-white mr-6 pt-4 mb-6">
-        <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg">
+        <img src="../../../assets/logo-white.png" class="px-10 w-full" />
+        <!-- svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg">
           <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
         </svg>
         <div class="flex flex-col items-start">
           <span class="text-xl select-none">Winkle</span>
-        </div>
+        </div-->
       </div>
       <div ref="userEl" class="flex justify-center flex-col items-center">
         <InitialsSquare
