e3064f7bba7bdc0ace9c0b12d2a33cc0c5eb4da5
lucas.souza <lucas.souza@bluecrux.com>
2022-03-07 12:07:18 -0300

    login screen remade

--- src/components/login/LoginScreen.vue
+++ src/components/login/LoginScreen.vue
@@ -6,63 +6,65 @@ defineProps<{ title: string }>();
 <template>
 <div class="h-screen w-screen flex justify-center bg-gray-300">
   <div class="lg:flex container">
-    <div class="lg:w-1/2 xl:max-w-screen-sm bg-white h-screen">
-      <div
+    <div
+      class="
+        lg:w-1/2 xl:max-w-screen-sm h-screen
+        bg-white
+        flex flex-col justify-center px-10 lg:px-16 xl:px-20
+      "
+    >
+      <router-link
+        tag="div"
         class="
-          pt-10
-          px-12
-          sm:px-24
-          md:px-48
-          lg:px-12
-          xl:px-24 xl:max-w-2xl
+          cursor-pointer
+          flex justify-center md:justify-start items-center
+          select-none drop-shadow
+        "
+        :to="{ name: 'login' }"
+      >
+        <img src="../../assets/logo-dark.png" class="w-20 px-0 mr-2" />
+        <div class="flex flex-col items-start text-gray-800">
+          <span class="text-5xl font-bold italic">Winkle</span>
+          <span class="text-sm ml-1">{{ $t('app-description') }}</span>
+        </div>
+      </router-link>
+
+      <h2
+        class="
+          text-center text-2xl text-gray-800
+          font-display font-semibold
+          lg:text-left my-4
         "
       >
-        <router-link
-          tag="div"
-          class="cursor-pointer flex items-center select-none drop-shadow"
-          :to="{ name: 'login' }"
+        {{ title }}
+      </h2>
+
+      <div class="mt-4">
+        <slot></slot>
+        <div
+          class="
+            mt-12 text-sm font-display font-semibold
+            text-gray-700 text-center
+          "
         >
-          <img src="../../assets/logo-dark.png" class="w-20 px-0 mr-2" />
-          <div class="flex flex-col items-start text-gray-800">
-            <span class="text-5xl font-bold italic">Winkle</span>
-            <span class="text-sm ml-1">{{ $t('app-description') }}</span>
-          </div>
-        </router-link>
-        <div class="mt-12 md:mt-16">
-          <slot></slot>
-          <div
-            class="
-              mt-12
-              text-sm
-              font-display font-semibold
-              text-gray-700 text-center
-            "
+          {{ $t('login.no-account') }}
+          <a
+            class="cursor-pointer text-indigo-600 hover:text-indigo-800"
+            href="https://www.winkle.com.br"
+            targett="_blank"
           >
-            {{ $t('login.no-account') }}
-            <a
-              class="cursor-pointer text-indigo-600 hover:text-indigo-800"
-              href="https://www.winkle.com.br"
-              targett="_blank"
-            >
-              {{ $t('login.sign-up') }}
-            </a>
-          </div>
+            {{ $t('login.sign-up') }}
+          </a>
         </div>
       </div>
     </div>
     <div
       class="
-        hidden
-        lg:flex
-        items-center
-        justify-center
-        flex-1
-        h-screen
+        hidden h-screen
+        lg:flex items-center justify-center flex-1
       "
     >
-      <div
-        class="max-w-xs transform duration-200 hover:scale-110 cursor-pointer"
-      >
+      <div class="max-w-xs transform duration-200 hover:scale-110 cursor-pointer">
         <WinkleImg />
       </div>
     </div>
@@ -78,7 +80,6 @@ defineProps<{ title: string }>();
   stroke-linecap: round;
   stroke-miterlimit: 3;
 }
-
 
 .slide-up-enter-active,
 .slide-up-leave-active {
